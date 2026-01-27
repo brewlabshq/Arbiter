06b3c4bf12bad2f039df52f1d0ea2ea3defaebec
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2026-01-27 10:16:58 +0530

    fix: arbiter state

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::sync::Arc;
 
 use crossbeam_queue::ArrayQueue;
+use solana_transaction::versioned::VersionedTransaction;
 
 use crate::{api::create_router, tx_auction::InternalTransaction};
 
 mod api;
 mod tx_auction;
 
+pub struct ArbiterState {
+    pub pending_tx: ArrayQueue<Vec<VersionedTransaction>>,
+    pub filted_tx: ArrayQueue<(Vec<VersionedTransaction>, u64)>
+}
 fn main() -> Result<(), anyhow::Error> {
     tracing_subscriber::fmt::init();
 
